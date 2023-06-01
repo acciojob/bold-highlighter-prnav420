f48e0978b75716f29a89de59e83e9cb7ca2c329a
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-06-01 09:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
+  //Write your code here
 
+  let getallstrong = document.getElementsByTagName("strong");
 
+  for (let i = 0; i < getallstrong.length; i++) {
+    getallstrong[i].style.color = "green";
+  }
 }
 
-
 function return_normal() {
-    //Write your code here
+  //Write your code here
 
-    
+  let getallstrong = document.getElementsByTagName("strong");
+
+  for (let i = 0; i < getallstrong.length; i++) {
+    getallstrong[i].style.color = "black";
+  }
 }
